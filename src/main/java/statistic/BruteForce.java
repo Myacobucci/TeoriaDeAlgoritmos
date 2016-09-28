1469900004ee18d53b181880858a713c616c90c1
Alex <bessoalex@gmail.com>
2016-09-28 20:41:16 -0300

    Estadísto orden k - Fuerza Bruta

---
+++ src/main/java/statistic/BruteForce.java
@@ -0,0 +1,7 @@
+package statistic;
+
+/**
+ * Created by alex on 28/09/16.
+ */
+public class BruteForce {
+}
---
+++ src/test/java/statistic/BruteForceTest.java
@@ -0,0 +1,8 @@
+import static org.junit.Assert.*;
+
+/**
+ * Created by alex on 28/09/16.
+ */
+public class BruteForceTest {
+
+}
