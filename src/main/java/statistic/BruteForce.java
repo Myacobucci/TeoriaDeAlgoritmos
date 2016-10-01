f42f4a9cc8d0f91d6d05108822141d5fe1786705
Alex <bessoalex@gmail.com>
2016-10-01 00:56:38 -0300

    BF intento 2

--- src/main/java/statistic/BruteForce.java
+++ src/main/java/statistic/BruteForce.java
@@ -1,7 +1,61 @@
 package statistic;
 
-/**
- * Created by alex on 28/09/16.
- */
+import java.util.ArrayList;
+import java.util.Iterator;
+
+    /**
+     * Created by alex on 24/09/16.
+     */
 public class BruteForce {
+
+    protected final ArrayList<Comparable> list = new ArrayList<Comparable>();;
+    protected int size;
+
+    public BruteForce(){
+        this.size = 0;
+    }
+    public BruteForce(ArrayList source){
+        this.list.addAll(source);
+        this.size = list.size();
+    }
+
+
+    public void addToList(Comparable e){
+        this.list.add(e);
+    }
+
+    public Comparable getFromList(int pos){
+        return list.get(pos);
+    }
+
+
+    public Object getSmallestK(int k){
+        boolean isKElement;
+        Iterator it = list.iterator();
+
+        while(it.hasNext()) {
+            Comparable elem = (Comparable) it.next();
+            if (isKElement(elem,k)){
+                return elem;
+            }
+        }
+        return null;
+    }
+
+    protected Boolean isKElement(Comparable element, int k) {
+        Iterator it = list.iterator();
+        int kPosition = 0;
+
+        while (it.hasNext()) {
+            Comparable actual = (Comparable) it.next();
+            if (actual.compareTo(element) < 0) {
+                kPosition++;
+            }
+        }
+
+        if (kPosition == k)
+            return true;
+        return false;
+    }
 }
+
--- src/test/java/statistic/BruteForceTest.java
+++ src/test/java/statistic/BruteForceTest.java
@@ -1,8 +1,32 @@
-import static org.junit.Assert.*;
+package statistic;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.ArrayList;
 
 /**
  * Created by alex on 28/09/16.
  */
 public class BruteForceTest {
+    @Test
+    public void getSmallestK() throws Exception {
+        ArrayList<Integer> list = new ArrayList<Integer>();
+
+        list.add(1);
+        list.add(10);
+        list.add(3);
+        list.add(4);
+        list.add(2);
+        list.add(0);
+
+        BruteForce bf = new BruteForce(list);
+
+        Assert.assertEquals(1, bf.getSmallestK(1));
+        Assert.assertEquals(3, bf.getSmallestK(3));
+        Assert.assertEquals(10, bf.getSmallestK(5));
+        Assert.assertNotEquals(4, bf.getSmallestK(5));
+
+    }
 
 }
