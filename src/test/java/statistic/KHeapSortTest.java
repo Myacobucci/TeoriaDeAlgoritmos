71ada8c074a85aaee65801ca7c73d1bce277a8f2
Ezequiel <ezequiel.devalais@gmail.com>
2016-10-02 16:14:25 -0300

    heapsort sample

---
+++ src/test/java/statistic/KHeapSortTest.java
@@ -0,0 +1,22 @@
+package statistic;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+/**
+ * Created by eze on 02/10/16.
+ */
+public class KHeapSortTest {
+    @Test
+    public void getKSmallest() throws Exception {
+
+        int []a1={4,1,3,2,16,9,10,14,8,7};
+        KHeapSort.sort(a1);
+        for(int i=0;i<a1.length;i++){
+            System.out.print(a1[i] + " ");
+        }
+    }
+
+}
