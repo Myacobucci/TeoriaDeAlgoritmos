c4ab6951e1adcabe4adf3daa3b0b213b9c8e7ebc
Ezequiel <ezequiel.devalais@gmail.com>
2016-10-03 21:08:05 -0300

    k heap sort working

--- src/main/java/statistic/KHeapSort.java
+++ src/main/java/statistic/KHeapSort.java
@@ -1,58 +1,71 @@
 package statistic;
 
-/**
- * Created by devalais on 02/10/16.
- */
+import java.util.ArrayList;
+
+
 public class KHeapSort {
 
-    private static int[] a;
-    private static int n;
-    private static int left;
-    private static int right;
-    private static int largest;
 
+    private int n;
+    private int left;
+    private int right;
+    private int largest;
+    protected int size;
+    public ArrayList<Comparable> a = new ArrayList<Comparable>();
 
-    public static void buildheap(int []a){
-        n=a.length-1;
+    public KHeapSort(ArrayList source){
+        this.a.addAll(source);
+        this.size = a.size();
+    }
+
+    public Object getSmallestK(int k){
+        int counter = 0;
+        buildHeap();
+        for(int i=n;i>0;i--){
+            exchange(0, i);
+            n=n-1;
+            maxHeap(a, 0);
+            if(counter == k) {
+                return a.get(size-1-k);
+            }
+            counter ++;
+        }
+        return a.get(size-1-k);
+    }
+
+    protected void buildHeap(){
+        n= size-1;
         for(int i=n/2;i>=0;i--){
-            maxheap(a,i);
+            maxHeap(a,i);
         }
     }
 
-    public static void maxheap(int[] a, int i){
+    public  void maxHeap(ArrayList<Comparable> a, int i){
         left=2*i;
         right=2*i+1;
-        if(left <= n && a[left] > a[i]){
-            largest=left;
+        if(left <= n && a.get(left).compareTo(a.get(i)) < 0){
+            largest = left;
         }
         else{
-            largest=i;
+            largest = i;
         }
-
-        if(right <= n && a[right] > a[largest]){
+        if(right <= n && a.get(right).compareTo(a.get(largest)) < 0){
             largest=right;
         }
         if(largest!=i){
             exchange(i,largest);
-            maxheap(a, largest);
+            maxHeap(a, largest);
         }
     }
 
-    public static void exchange(int i, int j){
-        int t=a[i];
-        a[i]=a[j];
-        a[j]=t;
+    public void exchange(int i, int j){
+        Comparable b = a.get(i);
+        Comparable c = a.get(j);
+        a.set(i,c);
+        a.set(j,b);
+
     }
 
-    public static void sort(int []a0){
-        a=a0;
-        buildheap(a);
 
-        for(int i=n;i>0;i--){
-            exchange(0, i);
-            n=n-1;
-            maxheap(a, 0);
-        }
-    }
 
 }
