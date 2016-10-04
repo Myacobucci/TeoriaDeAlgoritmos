2b59b119c78fba1ce7d39869f525f798190171eb
Ezequiel <ezequiel.devalais@gmail.com>
2016-10-03 21:08:05 -0300

    k heap sort working

--- src/test/java/statistic/KHeapSortTest.java
+++ src/test/java/statistic/KHeapSortTest.java
@@ -9,14 +9,29 @@ import java.util.ArrayList;
  * Created by eze on 02/10/16.
  */
 public class KHeapSortTest {
+
+
     @Test
     public void getKSmallestTest() throws Exception {
 
-        int []a1={4,1,3,2,16,9,10,14,8,7};
-        KHeapSort.sort(a1);
-        for(int i=0;i<a1.length;i++){
-            System.out.print(a1[i] + " ");
-        }
+        ArrayList<Integer> list = new ArrayList<Integer>();
+
+        list.add(1);
+        list.add(10);
+        list.add(3);
+        list.add(4);
+        list.add(2);
+        list.add(0);
+
+        KHeapSort hs = new KHeapSort(list);
+        Assert.assertEquals(1, hs.getSmallestK(1));
+        hs = new KHeapSort(list);
+        Assert.assertEquals(3, hs.getSmallestK(3));
+        hs = new KHeapSort(list);
+        Assert.assertEquals(10, hs.getSmallestK(5));
+        hs = new KHeapSort(list);
+        Assert.assertNotEquals(4, hs.getSmallestK(5));
+
     }
 
 }
